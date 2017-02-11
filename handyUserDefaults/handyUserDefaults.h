76a627bd6d8451bf16207091ad9b463406fda109
Edward Kim <onward.edkim@gmail.com>
2017-02-12 08:53:16 +1100

    Initial Commit

---
+++ handyUserDefaults/handyUserDefaults.h
@@ -0,0 +1,19 @@
+//
+//  handyUserDefaults.h
+//  handyUserDefaults
+//
+//  Created by KimYong Gyun on 12/2/17.
+//  Copyright © 2017 haruair. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for handyUserDefaults.
+FOUNDATION_EXPORT double handyUserDefaultsVersionNumber;
+
+//! Project version string for handyUserDefaults.
+FOUNDATION_EXPORT const unsigned char handyUserDefaultsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <handyUserDefaults/PublicHeader.h>
+
+
